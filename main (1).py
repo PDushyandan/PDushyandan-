faa7480b16cb5327e3e0b922246010e44d3c2a93
PDushyandan <147519783+PDushyandan@users.noreply.github.com>
2023-10-10 19:08:08 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,35 +1,8 @@
-class Player:
+ #Leap year
 
-    def play(self):
+year = int(input("Enter a year: "))
 
-        print("The player is playing cricket.")
-
- 
-
-class Batsman(Player):
-
-    def play(self):
-
-        print("The batsman is batting.")
-
- 
-
-class Bowler(Player):
-
-    def play(self):
-
-        print("The bowler is bowling.")
-
- 
-
-# Create objects of Batsman and Bowler classes and call the play() method for each object.
-
-batsman = Batsman()
-
-bowler = Bowler()
-
- 
-
-batsman.play()
-
-bowler.play()
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
--- main.py
+++ main.py
@@ -1,75 +1,13 @@
-class BankAccount:
+ #1.1 Implement a recursive function to calculate the factorial of a given number.
 
-    def __init__(self, account_number, account_holder_name, initial_balance):
+def factorial(n):
+    # Base case: factorial of 0 is 1
+    if n == 0:
+        return 1
+    # Recursive case: factorial of n is n times factorial of (n-1)
+    else:
+        return n * factorial(n - 1)
 
-        self.__account_number = account_number
-
-        self.__account_holder_name = account_holder_name
-
-        self.__account_balance = initial_balance
-
- 
-
-    def deposit(self, amount):
-
-        if amount > 0:
-
-            self.__account_balance += amount
-
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-
-        else:
-
-            print("Invalid deposit amount. Please deposit a positive amount.")
-
- 
-
-    def withdraw(self, amount):
-
-        if amount > 0 and amount <= self.__account_balance:
-
-            self.__account_balance -= amount
-
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-
-        else:
-
-            print("Invalid withdrawal amount or insufficient balance.")
-
- 
-
-    def display_balance(self):
-
-        print(f"Account Balance for {self.__account_holder_name} (Account #{self.__account_number}): ${self.__account_balance}")
-
- 
-
- 
-
-# Test the BankAccount class
-
-if __name__ == "__main__":
-
-    # Create an instance of BankAccount
-
-    my_account = BankAccount("123456789", "John Doe", 1000.0)
-
- 
-
-    # Test deposit and display_balance
-
-    my_account.display_balance()
-
-    my_account.deposit(500.0)
-
-    my_account.display_balance()
-
- 
-
-    # Test withdrawal and display_balance
-
-    my_account.withdraw(200.0)
-
-    my_account.display_balance()
-
-    my_account.withdraw(1500.0)  # This should result in an error message
+number = int(input("Enter number: "))
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
